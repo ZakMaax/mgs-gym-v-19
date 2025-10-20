36d71b542c7c92be817e5ad74a41b75c1bd49aa0
Said Maax <zacharia4499@gmail.com>
2025-10-20 14:07:09 +0300

    add gym class model

---
+++ addons-extra/mgs_gym/models/gym_class.py
@@ -0,0 +1,12 @@
+from odoo import models, fields  # type: ignore
+
+
+class GymShift(models.Model):
+    _name = "mgs_gym.class"
+    _description = "GYM Class"
+
+    name = fields.Char("name")
+    active = fields.Boolean(default=True)
+    shift_id = fields.Many2one("mgs_gym.shift", string="Shift")
+    coach_id = fields.Many2one("res.users", string="Coach", required=True)
+    description = fields.Html(string="Description")
